0d434dd9fcfc7224f5b4fbbcc12dcd0c4a1394d6
Mohammad Anar <ai.anarul4232@gmail.com>
2023-11-05 20:51:32 +0600

    mongodb successfully connected

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5001
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(cors())
+app.use(express.json())
 
 
 
-app.get('/', (req, res) => {
-    res.send("eStudypartners server is running");
-})
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zav38m0.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("eStudypartners server is running");
+});
 
 app.listen(port, () => {
-    console.log(`eStudy app is running form port: ${port}`);
-})
+  console.log(`eStudy app is running form port: ${port}`);
+});
