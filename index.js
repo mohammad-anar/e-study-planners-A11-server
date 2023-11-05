48f8963bc01c008094c103443c5ea0e8ab78c9f9
Mohammad Anar <ai.anarul4232@gmail.com>
2023-11-06 02:44:56 +0600

    assignment get and create api created

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+// parser 
+app.use(cors());
+app.use(express.json());
 
-
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zav38m0.mongodb.net/?retryWrites=true&w=majority`;
-
+// uri 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zav38m0.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,12 +23,35 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+// create collection 
+    const assignmentCollection = client
+      .db("groupStudyDB")
+      .collection("allAssignments");
+
+// get assginment apis 
+    app.get("/api/v1/assignments", async (req, res) => {
+        const result = await assignmentCollection.find().toArray();
+        res.send(result)
+    })
+    app.post("/api/v1/assignments", async (req, res) => {
+        const assignment = req.body || null
+        if(assignment) {
+            const result = await assignmentCollection.insertOne(assignment);
+           return res.send(result)
+        }
+        const result = await assignmentCollection.find().toArray();
+        res.send(result)
+    })
+
+
+
+
+// end apis 
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
