22c82ba937016236500a85dfae30b7d45d14dd72
Mohammad Anar <ai.anarul4232@gmail.com>
2023-11-06 03:59:27 +0600

    get api create find value by email

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// parser 
+// parser
 app.use(cors());
 app.use(express.json());
 
-// uri 
+// uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zav38m0.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -23,30 +23,47 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-// create collection 
+    // create collection
     const assignmentCollection = client
       .db("groupStudyDB")
       .collection("allAssignments");
 
-// get assginment apis 
-    app.get("/api/v1/assignments", async (req, res) => {
-        const result = await assignmentCollection.find().toArray();
-        res.send(result)
-    })
-    app.post("/api/v1/assignments", async (req, res) => {
-        const assignment = req.body || null
-        if(assignment) {
-            const result = await assignmentCollection.insertOne(assignment);
-           return res.send(result)
+      app.get("/api/v1/assignments", async (req, res) => {
+        const emailToSearch = req.query.email;
+        console.log(emailToSearch);
+        const query = {}
+        if (emailToSearch) {
+          query.email = emailToSearch;
         }
-        const result = await assignmentCollection.find().toArray();
-        res.send(result)
-    })
+      
+        const result = await assignmentCollection
+          .find(query)
+          .toArray();
+      
+        res.send(result);
+      });
 
+    app.get("/api/v1/assignments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {};
+      if (id) {
+        query._id = new ObjectId(id);
+      }
+      const result = await assignmentCollection.findOne(query);
+      res.send(result);
+    });
 
+    app.post("/api/v1/assignments", async (req, res) => {
+      const assignment = req.body || null;
+      if (assignment) {
+        const result = await assignmentCollection.insertOne(assignment);
+        return res.send(result);
+      }
+      const result = await assignmentCollection.find().toArray();
+      res.send(result);
+    });
 
-
-// end apis 
+    // end apis
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
